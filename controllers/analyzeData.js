afce58357a2431a26a06807e459c73249f35fc7e
Divya Thazhathethil <DivyaKalidasan.Thazhathethil@sjsu.edu>
2018-10-04 15:05:59 -0700

    draggable widget

--- controllers/analyzeData.js
+++ controllers/analyzeData.js
@@ -7,8 +7,6 @@ var states = xlsx.readFile('./public/data/states.xlsx').Sheets.Sheet1;
 module.exports.analyzedata = function(req,res) {
     var dataJsonArray = xlsx.utils.sheet_to_json(data).slice(0,10);
     var statesJsonArray = xlsx.utils.sheet_to_json(states);
-    res.render('../views/analyzeData');
-
     res.render('../views/analyzeData', {
         'data':dataJsonArray,
         'states':statesJsonArray
