bb127a51ece1e39f20c4e134523f5506a3c20f7d
Divya Thazhathethil <DivyaKalidasan.Thazhathethil@sjsu.edu>
2018-11-05 23:43:40 -0800

    pie chart

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -20,7 +20,8 @@ module.exports.fetchDataByState = function(req,res) {
     const cases = db.get('USData');
     var dataJsonArray = [];
     var dataAllYearsOfState = [];
-    var rentBurdenAllYears = []
+    var rentBurdenAllYears = [];
+    var poverty =[];
     cases.find(
             { "year":2016 }
 
@@ -35,19 +36,27 @@ module.exports.fetchDataByState = function(req,res) {
 
         cases.find({"name": selectedState}).then((results1)=>{
                 for(var i= 0; i < results1.length; i++){
-            dataAllYearsOfState.push({
-                    year: results1[i].year,
-                    value: results1[i].evictions
-                });
-            rentBurdenAllYears.push({
-                year: results1[i].year,
-                value: results1[i]["rent-burden"]
-            })
-            }
+                    dataAllYearsOfState.push({
+                            year: results1[i].year,
+                            value: results1[i].evictions
+                    });
+
+                    rentBurdenAllYears.push({
+                        year: results1[i].year,
+                        value: results1[i]["rent-burden"]
+                    });
+
+                    poverty.push({
+                        year: results1[i].year,
+                        poverty: results1[i]["poverty-rate"],
+                        population : results1[i]["population"]
+                    });
+                }
             res.render('../views/dashboard', {'mapData':dataJsonArray,
                 'yearData':dataAllYearsOfState,
                 'state': selectedState,
-                'rentBurden': rentBurdenAllYears
+                'rentBurden': rentBurdenAllYears,
+                'poverty' : poverty
             });
 
         });
